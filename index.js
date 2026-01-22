4a3565d68b9c84c53338cf95f20bd864e844345f
LinkyDev <linkyincdev@gmail.com>
2026-01-22 11:06:31 +0900

    버튼 액션 추가

---
+++ index.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const applyButton = document.querySelector('.apply-button');
+
+    applyButton.addEventListener('click', function() {
+        // 신청하기 버튼 클릭 시 동작
+        window.location.replace("https://study-apply.vercel.app/")
+    });
+});
